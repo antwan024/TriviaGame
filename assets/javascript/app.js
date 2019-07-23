8cec68b0859eba101b1888eb3f3367fc2fc90803
antwan024 <a.nguyen525i@gmail.com>
2019-07-23 12:47:27 -0600

    Got timer to change questions at zero.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,17 +8,10 @@ var questions = [
 var position = 0;
 
 var timedQuestion = function (questionArray) {
-
-    // for(var i=0; i<questions.length; i++) {
-
-    //     $("#question").text(questions[i][0]);
-    //     $("#choice").text()
-
-    // };
-
   
-    
-  
+    $("#question").empty();
+  $("#choice").empty();
+
     $("#question").text(questions[position][0]);
 
     var list = $("<ul>");
@@ -26,7 +19,7 @@ var timedQuestion = function (questionArray) {
         
         var choice = $("<li>");
         choice.attr("index", i);
-        choice.text(questions[0][i]);
+        choice.text(questions[position][i]);
         list.append(choice);
     };
 
@@ -38,13 +31,31 @@ timedQuestion(questions);
 
 
 
-var timeLeft = 30;
+var timeLeft = 10;
 
 var myTimer = function() {
   timeLeft--;
-  $("#timer").text(timeLeft);    
+  $("#timer").text(timeLeft);   
+  
+  if (timeLeft===0) {
+    clearInterval(interval);
+    timeLeft = 10;
+    position++;
+    timedQuestion(questions);
+  };
+  
+
+  
 };
 
 
-setInterval(myTimer, 1000);
+var interval = setInterval(myTimer, 1000);
+
+
+// if (timeLeft===0) {
+//   clearInterval(interval);
+//   timeLeft = 10;
+//   position++;
+//   timedQuestion(questions);
+// };
 

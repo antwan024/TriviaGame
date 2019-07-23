bfecf654bd8d1d0cf9483c1935fcd7cd738faf0b
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-07-22 22:06:39 -0600

    Added questions/answers array

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var questions = [
+    ["Who is The Rock?", "Michael Jackson", "Michael Jordan", "Elvis", "Dwayne Johnson", 3],
+    ["Who is the famous Taylor?", "Smith", "Swift", "Swimm", "Slappy", 1 ],
+    ["What is Michael Jordan Famous for?", "Football", "Squash", "Basketball", "Music", 2],
+    ["Who is Adam Sandler's famous character?", "Happy Gilmore", "Thor", "Iron Man", "John Wick", 0]
+    ];
+
+
+
+

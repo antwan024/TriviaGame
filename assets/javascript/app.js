00adc8dba86164a4c531c5218632bd1eab71e2e0
Anthony Nguyen <a.nguyen525i@gmail.com>
2019-08-15 22:33:02 -0600

    Finally corrected error on end of game and moved position variable to correct errors too.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,90 +13,86 @@ var points = 0;
 //nestQuestion will reset countdown and itterate up to the next question.
 var nextQuestion = function() {    
  
-  position++;
-  timeLeft = 10;
-  $("#timer").text(10); 
+    
+    console.log(position);
+    timeLeft = 10;
+    $("#timer").text(10); 
+    
 
-  if (position === questions.length) {
-    endGame();
-  } else {
-    // position++;
-  };
+    if (position === questions.length) {
+      endGame();
+    } else {
+      timedQuestion(questions); 
+    };
 
 };
 
 
 //timedQuestion will write to HTML, and call the event click for each li item.
-var timedQuestion = function (questionArray) {
+var timedQuestion = function (array) {
   
-  $("#question").empty();
-  $("#choice").empty();
+    $("#question").empty();
+    $("#choice").empty();
 
-  $("#question").text(questions[position][0]);
+    $("#question").text(array[position][0]);
 
-  var list = $("<ul id = 'choiceList'>");
-    
-  for (var i=1; i<questions[0].length-1; i++ ) {        
-    var choice = $("<li>");
-    var index = i-1;
-    choice.attr("id", "listChoice");
-    choice.attr("choice", index);
-    choice.attr("answer", questions[position][5]);
-    choice.text(questions[position][i]);
-    list.append(choice);
-  };
-
-  $("#choice").append(list);
-  
-
-
-  //will get click data and points and go to next question.
-  $("#choiceList").click(function(e){
-   
-    if(e.target && e.target.nodeName == "LI") {
-     
-      var answer = $(e.target).attr("answer");
-      var decision = $(e.target).attr("choice");
+    var list = $("<ul id = 'choiceList'>");
       
-      if (decision===answer){
-        points++;
-        $("#points").text(points);
-      };
-     
+    for (var i=1; i<array[0].length-1; i++ ) {        
+      var choice = $("<li>");
+      var index = i-1;
+      choice.attr("id", "listChoice");
+      choice.attr("choice", index);
+      choice.attr("answer", array[position][5]);
+      choice.text(array[position][i]);
+      list.append(choice);
     };
-  
-    console.log(position);
-    console.log(questions.length);
-    nextQuestion();
-    timedQuestion(questions); 
+
+    $("#choice").append(list);
     
-  });
-  
 
-  
+
+    //will get click data and points and go to next question.
+    $("#choiceList").click(function(e){
+    
+        position++;
+
+        if(e.target && e.target.nodeName == "LI") {
+          
+            var answer = $(e.target).attr("answer");
+            var decision = $(e.target).attr("choice");
+
+            console.log(e.target);
+            console.log(e.target.nodeName);
+            
+            if (decision===answer){
+              points++;
+              $("#points").text(points);
+            };
+          
+        };
+
+        console.log(questions.length);
+        nextQuestion();       
+      
+    });  
 };
-
-
 
 
 //Timer to countdown and go to next question.
 var myTimer = function() {
-  timeLeft--;
-  $("#timer").text(timeLeft);   
-  
-  if (timeLeft===0) {
-    nextQuestion();
-    timedQuestion(questions);
-  };
+    timeLeft--;
+    $("#timer").text(timeLeft);   
+    
+    if (timeLeft===0) {
+        position++;
+        nextQuestion();
+    };
 
 };
 
 
-
-
-
-
-$("#reset").click(function(e){
+$("#reset").click(function(){
    
     position = 0;
     points = 0;
@@ -106,17 +102,16 @@ $("#reset").click(function(e){
     timedQuestion(questions);
     interval = setInterval(myTimer, 1000);
     
-
 });
 
-        //ends the game and determines win/loss.
+
+//ends the game and determines win/loss.
 var endGame = function() {
   
     clearInterval(interval);
   
     var determineText = function(){
       
-
         if (points>=3) {
           $("#choice").text("You Win!");
         } else {
@@ -124,11 +119,11 @@ var endGame = function() {
         };
 
     };
-  
+
+    determineText();
   
     $("#question").text('');
-    $("#timer").text('done');
-    $("#choice").on(event.currentTarget,null, determineText);
+    $("#timer").text('done');    
 
 };
 
@@ -136,5 +131,3 @@ var endGame = function() {
 var interval = setInterval(myTimer, 1000);
 timedQuestion(questions);
 var timeLeft = 10;
-
-
